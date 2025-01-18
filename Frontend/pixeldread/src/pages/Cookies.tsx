45b56af6a8bcbe26feca1fc07369d54bbd119f97
Lukas <Luk4sp0p@gmail.com>
2025-01-18 18:11:03 +0100

    Refactor cookie and GDPR pages to enhance clarity and update language to English

--- Frontend/pixeldread/src/pages/Cookies.tsx
+++ Frontend/pixeldread/src/pages/Cookies.tsx
@@ -1,27 +1,27 @@
+import * as CookieConsent from "vanilla-cookieconsent";
 const Cookies = () => {
     return(
         <main>
-<header>
-	<h1>Zasady cookies</h1>
-	<p>Tato webová stránka používá cookies. Cookies jsou soubory, které jsou uloženy na vašem zařízení, když navštívíte webovou stránku. Tyto soubory pomáhají webové stránce identifikovat vás a vaše preference, což umožňuje webové stránce přizpůsobit se vašim potřebám.</p>
-</header>
-	<h2>Jaké cookies používáme?</h2>
-	<p>Na této webové stránce používáme cookies k ukládání vašich předvoleb a zlepšení výkonu webové stránky. Využíváme také cookies pro sledování používání webové stránky a zlepšení našich služeb</p>
-	<p>Typy cookies, které používáme</p>
-	<h3>1. Nezbytné cookies</h3>
-	<p>Tyto cookies jsou nezbytné pro správné fungování webové stránky. Bez nich byste nemohli využívat základní funkce webové stránky.</p>
-	<h3>2. Analytické cookies</h3>
-	<p>Analytické cookies nám umožňují sledovat používání webové stránky a zlepšovat naše služby. Tyto cookies shromažďují informace o tom, jak se naše webová stránka používá, abychom mohli zlepšovat uživatelskou zkušenost.</p>
-	<h3>3. Personalizované cookies</h3>
-	<p>Personalizované cookies slouží k poskytování personalizovaného obsahu a reklam. Tyto cookies mohou být vytvořeny třetími stranami a používají se k analýze vašeho používání webové stránky a zobrazování reklam na míru.</p>
-	<h2>Váš souhlas</h2>
-	<p>Používáním této webové stránky vyjadřujete souhlas se zpracováním vašich údajů v souladu s našimi zásadami cookies. Možnost omezit <button type="button" data-cc="show-preferencesModal">Manage cookie preferences</button>
-	. Pokud však zakážete cookies, může to omezit funkčnost webové stránky a omezit vaše zážitky z prohlížení webové stránky.</p>
-	<h2>Změny v naších zásadách cookies</h2>
-	<p>Můžeme aktualizovat naše zásady cookies v budoucnu. Pokud dojde k nějakým změnám, budeme tyto změny zveřejňovat na této stránce. Doporučujeme vám pravidelně kontrolovat tuto stránku pro aktualizace zásad cookies.</p>
-	<h2>Kontakt</h2>
-	<p>Pokud máte jakékoliv dotazy ohledně našich cookies, neváhejte mě kontaktovat prostřednictvím emailu <a href="mailto:jansebastiankostlan@gmail.com">jansebastiankostlan@gmail.com</a></p>
-</main>
+            <header>
+                <h1>Cookie Policy</h1>
+                <p>This website uses cookies. Cookies are files stored on your device when you visit a website. These files help the website identify you and your preferences, allowing the website to customize itself to your needs.</p>
+            </header>
+            <h2>What cookies do we use?</h2>
+            <p>On this website, we use cookies to store your preferences and improve the performance of the website. We also use cookies for tracking website usage and improving our services.</p>
+            <p>Types of cookies we use:</p>
+            <h3>1. Necessary Cookies</h3>
+            <p>These cookies are essential for the proper functioning of the website. Without them, you would not be able to use basic website functions.</p>
+            <h3>2. Analytical Cookies</h3>
+            <p>Analytical cookies allow us to track website usage and improve our services. These cookies collect information about how our website is used, helping us to enhance the user experience.</p>
+            <h3>3. Personalized Cookies</h3>
+            <p>Personalized cookies serve to provide customized content and advertisements. These cookies may be created by third parties and are used to analyze your usage of the website and show tailored advertisements.</p>
+            <h2>Your Consent</h2>
+            <p>By using this website, you consent to the processing of your data in accordance with our cookie policy. You can set your browser to limit or disable cookies. However, disabling cookies may limit the functionality of the website and restrict your browsing experience.</p>
+            <h2>Changes to Our Cookie Policy</h2>
+            <p>We may update our cookie policy in the future. If there are any changes, we will post those changes on this page. We recommend checking this page regularly for updates to the cookie policy.</p>
+            <h2>Contact</h2>
+            <p>If you have any questions regarding our cookies, feel free to contact me via email at <a href="mailto:jansebastiankostlan@gmail.com">jansebastiankostlan@gmail.com</a></p>
+        </main>
     )
 }
 export default Cookies;
--- Frontend/pixeldread/src/pages/Gdpr.tsx
+++ Frontend/pixeldread/src/pages/Gdpr.tsx
@@ -1,39 +1,43 @@
 import { Link } from "react-router-dom";
+
 const GDPR = () => {
     return (
         <main>
-        <header>
-            <h1>GDPR</h1>
-            <p>Prohlášení o souhlasu s GDPR.</p>
-        </header>
-        
-            <h2>Vaše údaje, Vaše práva</h2>
-                <p>
-                    Vítejte na stránce věnované souladu s GDPR na osobním webu Jana Sebastiána Kostlána. Toto prohlášení popisuje naše závazky chránit vaše soukromí a zajišťovat bezpečnost vašich osobních údajů v souladu s Nařízením o ochraně osobních údajů (GDPR).
-                </p>
-                <p>
-                    Respektujeme vaše právo na soukromí a kontrolu nad vašimi osobními údaji. Jako návštěvník našeho webu máte podle GDPR následující práva:
-                </p>
-                <ul>
-                    <li>Právo na přístup: Můžete požadovat přístup k vašim osobním údajům, které zpracováváme.</li>
-                    <li>Právo na opravu: Pokud jsou vaše osobní údaje nepřesné nebo neúplné, máte právo na jejich opravu nebo doplnění.</li>
-                    <li>Právo na výmaz: Můžete požadovat smazání svých osobních údajů za určitých okolností.</li>
-                    <li>Právo na námitku: Máte právo vznést námitku proti zpracování vašich osobních údajů pro určité účely.</li>
-                </ul>
-            <h2>Sběr a Použití Údajů</h2>
-                <p>
-                    Naše webové stránky sbírají a zpracovávají osobní údaje pouze pro explicitně uvedené účely. Vaše údaje nebudou bez vašeho souhlasu sdíleny s třetími stranami, s výjimkou případů, kdy to vyžaduje zákon.
-                </p>
-        
-                <h2>Politika Cookies</h2>
-                <p>
-                    Používáme soubory cookies ke zlepšení vašeho prohlížení. Používáním našeho webu souhlasíte s používáním cookies v souladu s naší <Link to="/cookies">politikou cookies</Link>.
-                </p>
-            <h2>Kontaktujte Nás</h2>
-                <p>
-                    Pokud máte nějaké otázky nebo obavy ohledně našeho souladu s GDPR nebo zpracování vašich osobních údajů, kontaktujte nás na <a href="mailto:jansebastiankostlan@gmail.com">jansebastiankostlan@gmail.com</a>.
-                </p>
+            <header>
+                <h1>GDPR</h1>
+                <p>Privacy Policy Statement.</p>
+            </header>
+
+            <h2>Your Data, Your Rights</h2>
+            <p>
+                Welcome to the page dedicated to GDPR compliance on Jan Sebastián Kostlán's personal website. This statement describes our commitments to protecting your privacy and ensuring the security of your personal data in accordance with the General Data Protection Regulation (GDPR).
+            </p>
+            <p>
+                We respect your right to privacy and control over your personal data. As a visitor to our website, you have the following rights under GDPR:
+            </p>
+            <ul>
+                <li>Right of access: You can request access to your personal data that we process.</li>
+                <li>Right of rectification: If your personal data is inaccurate or incomplete, you have the right to correct or supplement it.</li>
+                <li>Right of erasure: You can request the deletion of your personal data under certain circumstances.</li>
+                <li>Right to object: You have the right to object to the processing of your personal data for specific purposes.</li>
+            </ul>
+
+            <h2>Data Collection and Use</h2>
+            <p>
+                Our website collects and processes personal data only for explicitly stated purposes. Your data will not be shared with third parties without your consent, except in cases where required by law.
+            </p>
+
+            <h2>Cookies Policy</h2>
+            <p>
+                We use cookies to improve your browsing experience. By using our website, you consent to the use of cookies in accordance with our <Link to="/cookies">cookie policy</Link>.
+            </p>
+
+            <h2>Contact Us</h2>
+            <p>
+                If you have any questions or concerns regarding our GDPR compliance or the processing of your personal data, please contact us at <a href="mailto:jansebastiankostlan@gmail.com">jansebastiankostlan@gmail.com</a>.
+            </p>
         </main>
     );
-    }
-    export default GDPR;
+};
+
+export default GDPR;
