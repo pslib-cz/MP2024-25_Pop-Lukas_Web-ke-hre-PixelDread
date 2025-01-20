4f14fdb880f9ad1a3e97a92df433777050178fa5
Lukas <Luk4sp0p@gmail.com>
2025-01-20 19:04:55 +0100

    okay zase jsem dodrbal git, jak to sakra dělam

--- Backend/PixelDread/PixelDread/obj/Debug/net9.0/PixelDread.AssemblyInfo.cs
+++ Backend/PixelDread/PixelDread/obj/Debug/net9.0/PixelDread.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelDread")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13d6f9116b3d71229410de32178dbc28122ed74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96a23fceb78134a438ba69c0f638e8d371efb55")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelDread")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelDread")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
